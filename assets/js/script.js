09b5195789a10052fa55b797adb8092557122081
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2023-12-24 21:38:50 +0700

    add js and move any file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function moveToService() {
+    var konten = document.getElementById('service');
+    konten.scrollIntoView();
+}
+
+function moveToAboutUs() {
+    var konten = document.getElementById('tentang-kami');
+    konten.scrollIntoView();
+}
+
+function moveToTeam() {
+    var konten = document.getElementById('team');
+    konten.scrollIntoView();
+}
