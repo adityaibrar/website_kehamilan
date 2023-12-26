bff0a06a903fa4667fffdf90f2f2a01127c246e5
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2023-12-27 00:05:38 +0700

    add features diagnosa kehamilan but not finished

--- assets/js/diagnosakehamilan.js
+++ assets/js/diagnosakehamilan.js
@@ -1,4 +1,4 @@
-var title = document.querySelector('.service__container')
+var title = document.getElementById('service')
 var backNavigator = document.querySelector('.header');
 var trimester1 = document.getElementById('trimester1');
 var trimester2 = document.getElementById('trimester2');
@@ -16,7 +16,7 @@ function explore(){
 
 function trimester1UsiaKehamilan() {
     if (trimester1.style.display === 'none' || trimester1.style.display === '') {
-        trimester1.style.display = '';
+        trimester1.style.display = 'none';
         trimester2.style.display = 'none';
         trimester3.style.display = 'none';
         serviceGrid.style.display = 'none';
@@ -31,7 +31,7 @@ function trimester1UsiaKehamilan() {
 
 function trimester2UsiaKehamilan() {
     if (trimester2.style.display === 'none' || trimester2.style.display === '') {
-        trimester2.style.display = '';
+        trimester2.style.display = 'none';
         trimester1.style.display = 'none';
         serviceGrid.style.display = 'none';
         title.style.display = 'none';
@@ -45,11 +45,11 @@ function trimester2UsiaKehamilan() {
     }
 }
 
-function trimester3UsiaKehamilan() {
+function trimester3Diagnosa() {
     if (trimester3.style.display === 'none' || trimester3.style.display === '') {
-        trimester3.style.display = '';
-        trimester2.style.display = 'none';
-        trimester1.style.display = 'none';
+        trimester3.style.display = 'block';
+        // trimester2.style.display = 'none';
+        // trimester1.style.display = 'none';
         serviceGrid.style.display = 'none';
         title.style.display = 'none';
         btnUp.style.display = '';
